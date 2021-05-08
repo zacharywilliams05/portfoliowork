34ca40af671debdbfa09017758876ee940aff295
zacharywilliams05 <zachary.williams05@protonmail.com>
2021-05-07 19:47:34 -0700

    Added code for adding a user

--- python/week_5_python_script.py
+++ python/week_5_python_script.py
@@ -10,3 +10,28 @@ child.expect('.*password:')
 child.sendline('Password01')
 
 print('SSH login successful')
+
+#adding a user
+
+child.expect('.*\$')
+child.sendline('sudo adduser zackwilliams')
+child.expect('.*justincase:')
+child.sendline('Password01')
+child.expect('.*password:')
+child.sendline('dackarypilliams')
+child.expect('.*password:')
+child.sendline('dackarypilliams')
+child.expect('.*[]:')
+child.sendline('Zack Williams')
+child.expect('.*[]:')
+child.sendline('\r')
+child.expect('.*[]:')
+child.sendline('\r')
+child.expect('.*[]:')
+child.sendline('\r')
+child.expect('.*[]:')
+child.sendline('\r')
+child.expect('.*[Y/n]')
+child.sendline('\r')
+
+print('User successfully added')
