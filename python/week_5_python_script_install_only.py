2d001a888dcb4850a238a3f9fe3b0d41a48270cf
zacharywilliams05 <zachary.williams05@protonmail.com>
2021-05-09 10:36:09 -0700

    Added print lines

--- python/week_5_python_script_install_only.py
+++ python/week_5_python_script_install_only.py
@@ -2,6 +2,7 @@
 # By Zachary Williams
 # using python to install apache on Web1
 # 5/6/2021
+#code works
 
 #import
 from pexpect import pxssh
@@ -14,13 +15,16 @@ print('SSH Login Successful!')
 #codes to install
 session.prompt()
 session.sendline('sudo apt update')
+print('Doing the update')
 session.prompt()
 session.sendline('Password01')
+print('Sent password')
 session.prompt()
 session.sendline('sudo apt install apache2')
+print('Installing')
 session.prompt()
 session.sendline('y')
-
+print('Sent yes line')
 #enable auto-start apache
 session.prompt()
 session.sendline('sudo systemctl enable apache2')
