47475fd66a376516cf5d11c52ddddbccd3fc6efe
zacharywilliams05 <zachary.williams05@protonmail.com>
2021-05-09 10:22:07 -0700

    First attempt

--- python/week_5_python_script_install_only.py
+++ python/week_5_python_script_install_only.py
@@ -12,6 +12,16 @@ session.login('192.168.0.111','justincase', 'Password01')
 print('SSH Login Successful!')
 
 #codes to install
+session.prompt()
+session.sendline('sudo apt update')
+session.prompt()
+session.sendline('Password01')
+session.prompt()
+session.sendline('sudo apt install apache2')
+session.prompt()
+session.sendline('y')
 
-
+#enable auto-start apache
+session.prompt()
+session.sendline('sudo systemctl enable apache2')
 #exit
