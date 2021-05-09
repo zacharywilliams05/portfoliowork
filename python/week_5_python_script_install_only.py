960fc4478507797099d407c6991fa0f5a3f8127e
zacharywilliams05 <zachary.williams05@protonmail.com>
2021-05-09 10:42:34 -0700

    Web and custom print statement

--- python/week_5_python_script_install_only.py
+++ python/week_5_python_script_install_only.py
@@ -6,28 +6,31 @@
 
 #import
 from pexpect import pxssh
+web_ip = ['192.168.0.111', '192.168.0.112']
 
-#login
-session = pxssh.pxssh()
-session.login('192.168.0.111','justincase', 'Password01')
-print('SSH Login Successful!')
+#create for loop
+for i in range(web_ip):
+    #login
+    session = pxssh.pxssh()
+    session.login(i,'justincase', 'Password01')
+    print('SSH Login Successful to {0}'.format(i))
 
-#codes to install
-session.prompt()
-session.sendline('sudo apt update')
-print('Doing the update')
-session.prompt()
-session.sendline('Password01')
-print('Sent password')
-session.prompt()
-session.sendline('sudo apt install apache2')
-print('Installing')
-session.prompt()
-session.sendline('y')
-print('Sent yes line')
-#enable auto-start apache
-session.prompt()
-session.sendline('sudo systemctl enable apache2')
+    #codes to install
+    session.prompt()
+    session.sendline('sudo apt update')
+    print('Doing the update')
+    session.prompt()
+    session.sendline('Password01')
+    print('Sent password')
+    session.prompt()
+    session.sendline('sudo apt install apache2')
+    print('Installing')
+    session.prompt()
+    session.sendline('y')
+    print('Sent yes line')
+    #enable auto-start apache
+    session.prompt()
+    session.sendline('sudo systemctl enable apache2')
 
-print('Everything finished successfully!')
-#exit
+    print('Everything finished successfully!')
+    #exit
